cd636648c13bd78b638924bf3ce47e575bc523b5
kahono922 <jackson.kahono@student.moingaschool>
2022-09-06 02:19:26 +0300

    serve stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+// fetch('https://my-json-server.typicode.com/noela-murugi/JSON_API/doctors/1')
+// .then(response => response.json())
+// .then(data => {
+//       console.log(data)
+//       }
+// )
+
+//functions
+function distanceFromHqInBlocks(block){
+      let distance = block-42
+      return distance
+}
+
+
+console.log(distanceFromHqInBlocks(41))
